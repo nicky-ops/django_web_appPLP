573688427e1550d0c58c94d860f40ecb98ce510f
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-04-22 09:42:14 +0300

    add about page view

--- plpcohort3/home/views.py
+++ plpcohort3/home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def home_view(request):
+    posts = Post.objects.all()
+    context = {
+        "articles": posts
+    }
+    return render(request, "home.html", context)
+def about_view(request):
+    return render(request, "about.html")
