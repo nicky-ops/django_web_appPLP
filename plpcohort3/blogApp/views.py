4f8b862407f4f7c99530e33a49431b9b4a0447cd
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-05-03 08:22:33 +0300

    blog app views

--- plpcohort3/blogApp/views.py
+++ plpcohort3/blogApp/views.py
@@ -6,3 +6,22 @@ def home(request):
 
 def about_us(request):
     return render(request, 'about.html',{})
+
+posts = [
+    {
+        'author': 'Anna',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date': 'August 10, 2020'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date': 'August 1, 2020'
+    }
+]
+def article(request):
+    return render(request, 'post.html', {
+        "articles": posts,
+    })
