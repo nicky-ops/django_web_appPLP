4050000d2fd0a6184f1651bee01cf9fa736cd39e
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-05-03 16:58:29 +0300

    add articles url

--- plpcohort3/blogApp/urls.py
+++ plpcohort3/blogApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about_us, name = 'about_us'),
+    path('articles/', views.article, name = 'articles'),
 ]
