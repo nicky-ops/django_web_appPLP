e11c96c738700c5ab23b3b42db091bd68e035ecd
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-05-16 07:16:09 +0300

    modified url

--- plpcohort3/blogApp/urls.py
+++ plpcohort3/blogApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('home/', views.home, name = 'home'),
     path('about/', views.about_us, name = 'about_us'),
     path('articles/', views.article, name = 'articles'),
 ]
