68a6cb34028ce37cd958d7c5ece148ebc81908df
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-05-18 21:47:20 +0300

    used decorator to register models in the admin page

--- plpcohort3/blogApp/admin.py
+++ plpcohort3/blogApp/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class AdminPost(admin.ModelAdmin):
+    list_display = (
+        'title', 'content', 'date', 'author'
+    )
+    list_filter = (
+        'author', 'date'
+    )
+    list_max_show_all = 1
