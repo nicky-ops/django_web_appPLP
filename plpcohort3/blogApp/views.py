e2411d10f7c23062d548c28a82dac63151e92b75
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-05-19 20:32:47 +0300

    add new views

--- plpcohort3/blogApp/views.py
+++ plpcohort3/blogApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def home(request):
@@ -16,3 +17,40 @@ def article(request):
 
 def articles_details(request,pk):
     post = Post.objects.get(pk=pk)
+    context =  {
+        'post':post
+    }
+    return render(request, 'post_details.html', context)
+
+def article_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect(reverse('articles'))
+    else:
+        form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'create.html',context)
+
+def article_update(request, pk):
+    post_obj = Post.object.get(pk=pk)
+    if request.method == "POST":
+        form = PostForm(instance=post_obj, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('details', args=pk,))
+        else:
+            form = PostForm(instance=post_obj)
+        context = {
+            "post_obj":post_obj,
+            "form":form
+        }
+        return render(request, 'create.html',context)
+    
+    def article_delete(request, pk):
+        post_obj = get_object_or_404(Post, pk=pk)
+        post_obj.delete()
+        return redirect(reverse("articles"))
